e6310e4e89f782992ed657c7a3765f744a5fa74b
Max Appel <scheresteinpapier2.0@gmail.com>
2016-07-02 13:38:03 +0200

    Einige structs definiert
    Eingangsstructs definiert

--- Umgebungserkennung/Umgebungserkennung.ino
+++ Umgebungserkennung/Umgebungserkennung.ino
@@ -1,22 +1,30 @@
 void setup();
 void loop();
 String SerialInput();
+struct objekt umgebungserkennung();
 struct eingang{
   int winkel;
-  int abstand
+  int abstand;
+  int xkoor;
+  int ykoor;
+  struct eingang 
 };
 
-struct koordinaten{
+struct vektor{
   int xstrecke;
   int ystrecke;
   int winkel;
-}
+};
 
 struct objekt{
   int xent;
   int yent;
   int winkel;
-}
+  byte art;
+  int entfernung;
+};
+
+int startwinkel;
 
 
 void setup() {
@@ -24,5 +32,14 @@ void setup() {
 }
 
 void loop() {
-
+  struct objekt objekt = umgebungserkennung();
+  Serial.println(objekt.xent);
+  delay(500);
 }
+
+struct objekt umgebungserkennung()  {
+  //structinitialisierung;
+  struct eingang 0e,5e,10e,15e,20e,25e,30e,35e,40e,45e,50e,55e,60e,65e,70e,75e,80e,85e,90e,95e,100e,105e,110e,115e,120e,125,130e,135e,140e,145e,150e,155e,160e,165e,170e,175e,180e;
+  
+  }
+
